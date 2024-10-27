36b03d52ca975eea817b31108997eea5c689f388
PulkitPB <106659392+PulkitPB@users.noreply.github.com>
2024-10-27 16:49:40 +0530

    Update webpack.config.js

--- myproject/frontend/webpack.config.js
+++ myproject/frontend/webpack.config.js
@@ -1,66 +1,33 @@
 const path = require("path");
 const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: "production",  // Set Webpack to production mode
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    filename: "[name].[contenthash].js",  // Cache-busting with content hash
-    clean: true,  // Clean old build files
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/i,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
         },
       },
-      {
-        test: /\.css$/i,
-        exclude: /node_modules/,
-        use: [
-          MiniCssExtractPlugin.loader,  // Extracts CSS into separate files
-          "css-loader",
-          "postcss-loader",
-        ],
-      },
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,  // Remove console statements
-          },
-        },
-      }),
-      new CssMinimizerPlugin(),  // Minify CSS files
-    ],
-    splitChunks: {
-      chunks: "all",  // Split vendor and common code
-    },
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",  // Cache-busting for CSS
-    }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production"),  // Set environment variable to production
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
     }),
   ],
-  devtool: "source-map",  // Generate source maps for easier debugging
 };
